482800d7754fc37405b32814a43d6cc30a474720
Eric Nelson <eric.nelson@boundarydevices.com>
2013-05-10 10:02:23 -0700

    recovery: don't require UBIFS
    Change-Id: Idb248714f663557e3032ffe21a91894d3788579d
    Signed-off-by: Eric Nelson <eric.nelson@boundarydevices.com>

--- Android.mk
+++ Android.mk
@@ -54,10 +54,13 @@ LOCAL_CFLAGS += -DUSE_EXT4
 LOCAL_C_INCLUDES += system/extras/ext4_utils
 LOCAL_STATIC_LIBRARIES += libext4_utils_static libz
 
+ifeq ($(TARGET_USERIMAGES_USE_UBIFS),true)
+$(warning -----------got here)
 LOCAL_CFLAGS += -DUSE_UBIFS
 LOCAL_C_INCLUDES += external/mtd-utils/new-utils/include/
 LOCAL_STATIC_LIBRARIES += libubi
 LOCAL_SRC_FILES += ubi.cpp
+endif
 
 ifeq ($(HAVE_SELINUX), true)
   LOCAL_C_INCLUDES += external/libselinux/include
