63a3d3b7ec76bc9f659aabc2c5da854677b612c6
zhang sanshan <b51434@freescale.com>
2014-11-25 17:01:15 +0800

    MA-5899 fix sabreauto_6q lollipop nand compile error
    be lack of cryptfs.h when compile lollipop nand on sabreauto_6q
    fatal error: cryptfs.h: No such file or directory
    
    Signed-off-by: zhang sanshan <b51434@freescale.com>
    
    Conflicts:
    	Android.mk

--- Android.mk
+++ Android.mk
@@ -83,13 +83,13 @@ LOCAL_STATIC_LIBRARIES := \
 
 ifeq ($(TARGET_USERIMAGES_USE_EXT4), true)
     LOCAL_CFLAGS += -DUSE_EXT4
-    LOCAL_C_INCLUDES += system/extras/ext4_utils
+    LOCAL_C_INCLUDES += system/extras/ext4_utils 
     LOCAL_STATIC_LIBRARIES += libext4_utils_static libz
 endif
 
 LOCAL_MODULE_PATH := $(TARGET_RECOVERY_ROOT_OUT)/sbin
 LOCAL_CFLAGS += -DUSE_UBIFS
-LOCAL_C_INCLUDES += external/mtd-utils/new-utils/include/
+LOCAL_C_INCLUDES += external/mtd-utils/new-utils/include/ system/vold
 LOCAL_STATIC_LIBRARIES += libubi
 LOCAL_SRC_FILES += ubi.cpp
 
